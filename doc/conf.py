d09f5eb02d0283ca3b9535681cbebf5c565e3b86
Aubrey Dugger <adugger@ucar.edu>
2023-09-14 20:59:43 -0600

    new logo

--- doc/conf.py
+++ doc/conf.py
@@ -85,13 +85,15 @@ todo_include_todos = False
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = "ral_logo.png"
+html_logo = "climate_primer_small.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {}
+html_theme_options = {
+'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
